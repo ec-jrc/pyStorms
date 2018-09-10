afa52e86f80a171d926160b6e92006cf9d76224a
brey <breyiannis@gmail.com>
2018-09-10 10:02:15 +0200

    Add headers

--- Storms/mc.py
+++ Storms/mc.py
@@ -1,4 +1,13 @@
-"Function performing an estimation of the Holland parameters based on Monte Carlo"
+"""
+Function performing an estimation of the Holland parameters based on Monte Carlo
+
+"""
+# Copyright 2018 European Union
+# This file is part of [software name], a software written by [author's name] ([JRC unit])
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence").
+# Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the Licence for the specific language governing permissions and limitations under the Licence. 
+
 
 import numpy as np
 
